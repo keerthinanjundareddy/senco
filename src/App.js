f20612aff52b3807a7ed366b668d6a80e1768537
keerthinanjundareddy <keerthana@apprikart.com>
2023-11-07 00:03:52 +0530

    one

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react'
-import Thanks from './Thanks'
+import Thankstwo from './Thankstwo'
 
 function App() {
   return (
     <>
-      <Thanks />
+      {/* <Thanks /> */}
+      <Thankstwo />
     </>
   )
 }
--- src/Thankstwo.js
+++ src/Thankstwo.js
@@ -1,44 +1,39 @@
 import React from 'react'
-import './Thanks.css'
+import './Thankstwo.css'
 import logo from './Assets/logo with website.png'
 import lakshmicoin from './Assets/Group 4.png'
 import webicon from './Assets/web-icon.png'
-
-function Thanks() {
+function Customgridlayout() {
     function openwebLink(){
        
         window.open('https://sencogoldanddiamonds.com/', '_blank'); 
     }
   return (
     <>
-    <div className='full-contianer'>
-        <div className='navbar-section'>
-
-            <div className='logo'><img src={logo} alt="logo" className='logoicon'  /></div>
+     <div className='homepage-container'>
+      <div className='content1'>
+       <div className='navbar-section'> 
+       <div className='logo'><img src={logo} alt="logo" className='logoicon'  /></div>
            
-            <div className='navbar-text' onClick={openwebLink}><span className='visit-text'>visit senco website</span><span className='web-text'><img src={webicon}alt="webicon" /></span></div>
-            
-            
-
-        </div>
-        <div className='whole-section'>
+           <div className='navbar-text' onClick={openwebLink}><span className='visit-text'>Visit Senco Website</span><span className='web-text'><img src={webicon}alt="webicon" /></span></div>
+        </div> 
+         </div> 
+      <div className='content2' >
+  
             <div className='coin-logo'><img src={lakshmicoin} alt="lakshmi-coin" className='lakshmicon'/></div>
             <div className='thank-you-text'>Thank You!</div>
             <div className='succes-text'>We've successfully received your registration. </div>
             <div className='email-notification-text'>You'll be notified by email once your personalised prayer<br /> video for Ma Lakshmi is ready </div>
             <div className='end-diwali-text'>We at Senco wish you and your family a very happy <br /> Dhanteras and Diwali.</div>
-            </div>
-        <div className='footer-section'> 
-
-            <div className='footer-text'>*Terms and conditions apply</div>
-
-            </div>
-       
+           
+      </div>
+      <div className='content3'>*Terms and conditions apply</div>
+   
       
     </div>
     </>
   )
 }
 
-export default Thanks
+export default Customgridlayout
 
